2b13868fbd641046c616b34d40f9d225faced37e
Greta Moseley <greta.moseley@gmail.com>
2017-10-11 10:18:13 -0500

    initial setup for training programs

---
+++ migrations/20171011144208-create-training.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('trainings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      start_date: {
+        type: Sequelize.DATEONLY
+      },
+      end_date: {
+        type: Sequelize.DATEONLY
+      },
+      max_attendees: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('trainings');
+  }
+};
