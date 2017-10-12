23e8c38d6d924f9987bfc5923a9e9032572418e4
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-12 11:30:27 -0500

    seed the employee training join table

--- seeders/20171012162012-emps-training-join.js
+++ seeders/20171012162012-emps-training-join.js
@@ -1,26 +1,13 @@
 'use strict';
 
+const { employeeTrainings } = require('./data/employee-training');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('EmployeeTrainings', employeeTrainings, {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('EmployeeTrainings', null, {});
   }
 };
